9826c8f42c8f6e6512c4ca9f938d6bd8b410c8a3
rowan-py <alexandre.medeiros1@escolar.ifrn.edu.br>
2026-09-18 14:23:50 -0300

    alterei o html

--- app9.js
+++ app9.js
@@ -1,5 +1,7 @@
 const amigos = ["Alexandre", "Bruno", "Carlos", "Daniela", "Eduardo"]; 
 tam = amigos.length;
+
+console.log(`Arranjo: ${amigos}`);
 console.log ("O tamanho do array é: " + tam);
 console.log ("O primeiro amigo é: " + amigos[0]);
 console.log ("O último amigo é: " + amigos[tam - 1]);
