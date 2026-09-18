ef2b53db4086f811b83dfccc7aa931413d94e853
rowan-py <alexandre.medeiros1@escolar.ifrn.edu.br>
2026-09-18 14:11:07 -0300

    atualizei o index

---
+++ app9.js
@@ -0,0 +1,5 @@
+const amigos = ["Alexandre", "Bruno", "Carlos", "Daniela", "Eduardo"]; 
+tam = amigos.length;
+console.log ("O tamanho do array é: " + tam);
+console.log ("O primeiro amigo é: " + amigos[0]);
+console.log ("O último amigo é: " + amigos[tam - 1]);
